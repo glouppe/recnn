957209468943b51473b2ec2fb8611af9dc727ddf
Gilles Louppe <g.louppe@gmail.com>
2016-10-05 18:04:15 +0200

    join learning

--- train_join.py
+++ train_join.py
@@ -14,10 +14,8 @@ from recnn.preprocessing import permute_by_pt
 from recnn.preprocessing import extract
 from recnn.recnn import log_loss
 from recnn.recnn import adam
-from recnn.recnn import grnn_init_simple
-from recnn.recnn import grnn_predict_simple
-from recnn.recnn import grnn_init_gated
-from recnn.recnn import grnn_predict_gated
+from recnn.recnn import grnn_init_simple_join
+from recnn.recnn import grnn_predict_simple_join
 
 
 logging.basicConfig(level=logging.INFO,
@@ -25,32 +23,31 @@ logging.basicConfig(level=logging.INFO,
 
 
 @click.command()
-@click.argument("filename_train")
+@click.argument("filename_train1")
+@click.argument("filename_train2")
 @click.argument("filename_model")
-@click.option("--simple", is_flag=True, default=False)
 @click.option("--n_features", default=7)
-@click.option("--n_hidden", default=30)
+@click.option("--n_hidden", default=40)
 @click.option("--n_epochs", default=5)
 @click.option("--batch_size", default=64)
-@click.option("--step_size", default=0.01)
-@click.option("--decay", default=0.7)
+@click.option("--step_size", default=0.005)
+@click.option("--decay", default=0.9)
 @click.option("--random_state", default=1)
-def train(filename_train,
+def train(filename_train1,
+          filename_train2,
           filename_model,
-          simple=False,
           n_features=7,
-          n_hidden=30,
+          n_hidden=40,
           n_epochs=5,
           batch_size=64,
-          step_size=0.01,
-          decay=0.7,
+          step_size=0.0005,
+          decay=0.9,
           random_state=1):
     # Initialization
-    gated = not simple
     logging.info("Calling with...")
-    logging.info("\tfilename_train = %s" % filename_train)
+    logging.info("\tfilename_train1 = %s" % filename_train1)
+    logging.info("\tfilename_train2 = %s" % filename_train2)
     logging.info("\tfilename_model = %s" % filename_model)
-    logging.info("\tgated = %s" % gated)
     logging.info("\tn_features = %d" % n_features)
     logging.info("\tn_hidden = %d" % n_hidden)
     logging.info("\tn_epochs = %d" % n_epochs)
@@ -63,64 +60,74 @@ def train(filename_train,
     # Make data
     logging.info("Loading data...")
 
-    fd = open(filename_train, "rb")
-    X, y = pickle.load(fd)
+    fd = open(filename_train1, "rb")
+    X1, y = pickle.load(fd)
     fd.close()
     y = np.array(y)
 
-    indices = rng.permutation(len(X))
-    size = min(80000, len(X))
-    X = [X[i] for i in indices[:size]]
-    y = [y[i] for i in indices[:size]]
+    fd = open(filename_train2, "rb")
+    X2, _ = pickle.load(fd)
+    fd.close()
 
-    logging.info("\tfilename = %s" % filename_train)
-    logging.info("\tX size = %d" % len(X))
+    indices = rng.permutation(len(X1))
+    size = min(80000, len(X1))
+    X1 = [X1[i] for i in indices[:size]]
+    X2 = [X2[i] for i in indices[:size]]
+    y = [y[i] for i in indices[:size]]
+    y = np.array(y)
+
+    logging.info("\tfilename = %s" % filename_train1)
+    logging.info("\tfilename = %s" % filename_train2)
+    logging.info("\tX1 size = %d" % len(X1))
+    logging.info("\tX2 size = %d" % len(X2))
     logging.info("\ty size = %d" % len(y))
 
     # Preprocessing
     logging.info("Preprocessing...")
-    X = [extract(permute_by_pt(jet)) for jet in X]
-    tf = RobustScaler().fit(np.vstack([jet["content"] for jet in X]))
 
-    for jet in X:
+    X1 = [extract(permute_by_pt(jet)) for jet in X1]
+    tf = RobustScaler().fit(np.vstack([jet["content"] for jet in X1]))
+
+    for jet in X1:
+        jet["content"] = tf.transform(jet["content"])
+
+    X2 = [extract(permute_by_pt(jet)) for jet in X2]
+    tf = RobustScaler().fit(np.vstack([jet["content"] for jet in X2]))
+
+    for jet in X2:
         jet["content"] = tf.transform(jet["content"])
 
     # Split into train+test
     logging.info("Splitting into train and validation...")
 
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y,
-                                                          test_size=5000,
-                                                          random_state=rng)
+    X1_train, X1_valid, X2_train, X2_valid, y_train, y_valid = train_test_split(X1, X2, y, test_size=5000, random_state=rng)
 
     # Training
     logging.info("Training...")
 
-    if gated:
-        predict = grnn_predict_gated
-        init = grnn_init_gated
-    else:
-        predict = grnn_predict_simple
-        init = grnn_init_simple
+    predict = grnn_predict_simple_join
+    init = grnn_init_simple_join
 
     trained_params = init(n_features, n_hidden, random_state=rng)
-    n_batches = int(np.ceil(len(X_train) / batch_size))
+    n_batches = int(np.ceil(len(X1_train) / batch_size))
     best_score = [-np.inf]  # yuck, but works
     best_params = [trained_params]
 
-    def loss(X, y, params):
-        y_pred = predict(params, X)
+    def loss(X1, X2, y, params):
+        y_pred = predict(params, X1, X2)
         l = log_loss(y, y_pred).mean()
         return l
 
     def objective(params, iteration):
         rng = check_random_state(iteration % n_batches)
-        start = rng.randint(len(X_train) - batch_size)
+        start = rng.randint(len(X1_train) - batch_size)
         idx = slice(start, start+batch_size)
-        return loss(X_train[idx], y_train[idx], params)
+        return loss(X1_train[idx], X2_train[idx], y_train[idx], params)
 
     def callback(params, iteration, gradient):
         if iteration % 25 == 0:
-            roc_auc = roc_auc_score(y_valid, predict(params, X_valid))
+            roc_auc = roc_auc_score(y_valid,
+                                    predict(params, X1_valid, X2_valid))
 
             if roc_auc > best_score[0]:
                 best_score[0] = roc_auc
@@ -134,8 +141,10 @@ def train(filename_train,
                 "%5d\t~loss(train)=%.4f\tloss(valid)=%.4f"
                 "\troc_auc(valid)=%.4f\tbest_roc_auc(valid)=%.4f" % (
                     iteration,
-                    loss(X_train[:5000], y_train[:5000], params),
-                    loss(X_valid, y_valid, params),
+                    loss(X1_train[:5000], X2_train[:5000],
+                         y_train[:5000], params),
+                    loss(X1_valid, X2_valid,
+                         y_valid, params),
                     roc_auc,
                     best_score[0]))
 
